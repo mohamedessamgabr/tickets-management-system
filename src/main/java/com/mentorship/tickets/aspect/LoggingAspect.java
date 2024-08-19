baa20eb917c78beece097c40acecd8256f7abb8a
Mohamed Essam Eldin <mesam148@gmail.com>
2024-08-19 04:16:43 +0300

    Add Logging Aspect

---
+++ src/main/java/com/mentorship/tickets/aspect/LoggingAspect.java
@@ -0,0 +1,23 @@
+package com.mentorship.tickets.aspect;
+
+import lombok.extern.log4j.Log4j2;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Log4j2
+public class LoggingAspect {
+
+    @Around("execution(* com.mentorship.controller..*(..))")
+    public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+        Object proceed = joinPoint.proceed();
+        long executionTime = System.currentTimeMillis() - start;
+        log.info("{} executed in {} ms", joinPoint.getSignature(), executionTime);
+        System.out.println(joinPoint.getSignature() + " executed in " + executionTime + "ms");
+        return proceed;
+    }
+}
